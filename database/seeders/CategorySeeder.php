2d53f272aaac6a0ff87443681e1e73553e2c30cc
Slavo Kozar <slavo.kozar@gmail.com>
2021-02-23 15:54:34 +0100

    database seeders

---
+++ database/seeders/CategorySeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Category;
+use Illuminate\Database\Seeder;
+
+class CategorySeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $c = new Category;
+        $c->name = 'New from seeder';
+        $c->save();
+    }
+}
