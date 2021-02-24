9a168eeb4dfff89a09c2dbdf887a16fbf5828b48
Slavo Kozar <slavo.kozar@gmail.com>
2021-02-24 10:09:15 +0100

    added reservation -> user relationship

--- app/Models/Reservation.php
+++ app/Models/Reservation.php
@@ -19,4 +19,9 @@ class Reservation extends Model
     {
         return $this->belongsTo(Book::class);
     }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
