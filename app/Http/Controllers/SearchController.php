84e95672e78baaf170d2ce0f0f138cdc7c72ebd2
j_polak <jan.polak@onmyway.cz>
2021-02-26 12:02:20 +0100

    additional search features

--- app/Http/Controllers/SearchController.php
+++ app/Http/Controllers/SearchController.php
@@ -11,10 +11,55 @@ class SearchController extends Controller
     /**
      * search within books
      */
-    public function books()
+    public function books(Request $request)
     {
-        $books = Book::orderBy('id')->paginate(20);
+        //                                                 default value
+        $items_on_page = $request->input('results_on_page', 20);
 
-        return view('search.results', compact('books'));
+        $orderby = $request->input('orderby', 'name_ascending');
+
+        switch ($orderby) {
+            default:
+            case 'name_ascending':
+                $order_column = 'title';
+                $order_direction = 'asc';
+                break;
+            case 'name_descending':
+                $order_column = 'title';
+                $order_direction = 'desc';
+                break;
+            case 'id_ascending':
+                $order_column = 'id';
+                $order_direction = 'asc';
+                break;
+            case 'id_descending':
+                $order_column = 'id';
+                $order_direction = 'desc';
+                break;
+        }
+
+        $search_term = $request->input('search');
+
+        // start building the query, get the query builder object
+        $query_builder = Book::orderBy($order_column, $order_direction);
+
+        if ($search_term !== null) {
+            $query_builder->where('title', 'like', '%' . $search_term . '%');
+        }
+
+        // actually run the query and get the results
+        $books = $query_builder->paginate($items_on_page)->withQueryString();
+
+
+        $display_as_list = $request->get('display_as_list');
+        $display_as_grid = $request->get('display_as_grid');
+        if ($display_as_grid) {
+            $display_as = 'grid';
+        } else {
+            $display_as = 'list';
+        }
+
+
+        return view('search.results', compact('books', 'items_on_page', 'orderby', 'search_term', 'display_as'));
     }
 }
--- resources/views/search/results.blade.php
+++ resources/views/search/results.blade.php
@@ -7,11 +7,44 @@
 
         <h1>Search results</h1>
 
+        <div class="search-results__options">
+
+            {{-- <form action="{{ action('SearchController@books') }}" method="get"> --}}
+            <form action="" method="get">
+
+                <input type="hidden" name="page" value="{{ $books->currentPage() }}">
+
+                <select name="results_on_page">
+                    <option value="10" {{ $items_on_page == 10 ? 'selected' : '' }}>10</option>
+                    <option value="20" {{ $items_on_page == 20 ? 'selected' : '' }}>20</option>
+                    <option value="50" {{ $items_on_page == 50 ? 'selected' : '' }}>50</option>
+                    <option value="100" {{ $items_on_page == 100 ? 'selected' : '' }}>100</option>
+                </select>
+
+                <button>Set number of results per page</button>
+
+                <select name="orderby">
+                    <option value="name_ascending" {{ $orderby == 'name_ascending' ? 'selected' : '' }}>Name ascending</option>
+                    <option value="name_descending" {{ $orderby == 'name_descending' ? 'selected' : '' }}>Name descending</option>
+                </select>
+
+                <button>Set ordering</button>
+
+                <input type="text" name="search" value="{{ $search_term }}">
+
+                <button>Search</button>
+
+                <input type="submit" name="display_as_list" value="Display as list">
+                <input type="submit" name="display_as_grid" value="Display as grid">
+            </form>
+
+        </div>
+
         <div class="search-results__pagination">
             {{ $books->links('vendor.pagination.default') }}
         </div>
 
-        <div class="search-results__results">
+        <div class="search-results__results {{ $display_as == 'grid' ? 'search-results__results--grid' : '' }}">
 
             @foreach ($books as $book)
 
